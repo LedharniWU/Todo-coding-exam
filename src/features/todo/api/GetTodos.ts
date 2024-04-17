634cbd49a03312d441323000b26ed540f615aa34
Ledharni <LedharniWu@gmail.com>
2024-04-17 10:31:42 +0900

    update GetTodos

--- src/features/todo/api/GetTodos.ts
+++ src/features/todo/api/GetTodos.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
-import { Todo } from '../types/types'
+import { Todo, errorMessage } from '../types/types'
 
 type TodosResponse = Todo[]
 
-export async function GetTodos(): Promise<TodosResponse | undefined> {
+export async function GetTodos(): Promise<TodosResponse | errorMessage> {
   try {
     const rawResponse = await axios.get<TodosResponse>(
       'http://localhost:3000/api/todos',
@@ -16,17 +16,20 @@ export async function GetTodos(): Promise<TodosResponse | undefined> {
         switch (error.response.status) {
           case 500:
             console.error('予期しないエラー')
-            break
+            return { errorMessage: '予期しないエラー 500' }
           default:
             console.error(`エラーが発生しました: ${error.response.status}`)
+            return {
+              errorMessage: `エラーが発生しました: ${error.response.status}`,
+            }
         }
       } else {
         console.error('ネットワーク通信エラー')
+        return { errorMessage: 'ネットワーク通信エラー' }
       }
     } else {
       console.error('予期しないエラーが発生しました。:', error)
+      return { errorMessage: `予期しないエラーが発生しました。: ${error}` }
     }
   }
-
-  return undefined
 }
